14bb529603a2736da67aa9b3bb7b2cf80081c973
Diego P <89942112+diegoPonton@users.noreply.github.com>
2023-12-16 14:43:19 -0500

    Update index.py

--- PracticaLeccionMatrices/index.py
+++ PracticaLeccionMatrices/index.py
@@ -107,6 +107,87 @@ print(arrTiendas[combined])
 
 tiendasVentas = zip(arrTiendas, ventasPorTienda)
 
+
+
+# e) Calcule el promedio de ventas por categoría (fútbol y natación)
+promedioVentasFutbol = np.mean(matriz_valores[:, :3])
+promedioVentasNatacion = np.mean(matriz_valores[:, 3:])
+
+print("Promedio de ventas en la categoría fútbol:", promedioVentasFutbol)
+print("Promedio de ventas en la categoría natación:", promedioVentasNatacion)
+
+# f) Encuentre la tienda que ha tenido la venta más baja en la categoría fútbol
+ventaMinFutbol = np.min(matriz_valores[:, :3])
+tiendaMinFutbol = tiendas[np.argmin(matriz_valores[:, :3])]
+
+print("La tienda con la venta más baja en la categoría fútbol es:", tiendaMinFutbol)
+print("Monto de la venta más baja en la categoría fútbol:", ventaMinFutbol)
+
+# g) Encuentre la categoría (fútbol o natación) que ha tenido la venta total más alta
+totalVentasFutbol = np.sum(matriz_valores[:, :3])
+totalVentasNatacion = np.sum(matriz_valores[:, 3:])
+
+categoriaVentaMaxima = "fútbol" if totalVentasFutbol > totalVentasNatacion else "natación"
+ventaMaxima = max(totalVentasFutbol, totalVentasNatacion)
+
+print("La categoría con la venta total más alta es:", categoriaVentaMaxima)
+print("Monto de la venta total más alta:", ventaMaxima)
+
+
 arrFiltrado = list(filter(lambda tienda: tienda[1] > 4000 and tienda[1] < 6000, tiendasVentas))
 
 print(arrFiltrado)
+
+
+
+# h) Encuentre y muestre la matriz transpuesta de la matriz_valores
+matriz_transpuesta = np.transpose(matriz_valores)
+print("Matriz Transpuesta:")
+print(matriz_transpuesta)
+
+# i) Encuentre y muestre la matriz resultante de multiplicar la matriz_valores por 2
+matriz_doble = matriz_valores * 2
+print("Matriz Doble:")
+print(matriz_doble)
+
+# j) Calcule y muestre la suma de cada columna en la matriz_valores
+suma_columnas = np.sum(matriz_valores, axis=0)
+print("Suma de cada columna:")
+print(suma_columnas)
+
+# k) Calcule y muestre la suma de cada fila en la matriz_valores
+suma_filas = np.sum(matriz_valores, axis=1)
+print("Suma de cada fila:")
+print(suma_filas)
+
+# l) Encuentre la tienda que ha tenido la mayor venta en productos de natación
+venta_maxima_natacion = np.max(matriz_valores[:, 3:])
+tienda_maxima_natacion = tiendas[np.argmax(matriz_valores[:, 3:])]
+
+print("La tienda con la mayor venta en produ
+
+
+
+# m) Encuentre y muestre la cantidad de productos vendidos en total por cada categoría (fútbol y natación)
+# m) Encuentre y muestre la cantidad de productos vendidos en total por cada categoría (fútbol y natación)
+total_productos_futbol = np.sum(matriz_valores[:, :3])
+total_productos_natacion = np.sum(matriz_valores[:, 3:])
+
+print("Total de productos vendidos en la categoría fútbol:", total_productos_futbol)
+print("Total de productos vendidos en la categoría natación:", total_productos_natacion)
+
+# n) Encuentre y muestre la tienda que ha tenido la mayor venta total
+venta_total_tienda = np.sum(matriz_valores, axis=1)
+tienda_maxima_venta_total = tiendas[np.argmax(venta_total_tienda)]
+
+print("La tienda con la mayor venta total es:", tienda_maxima_venta_total)
+print("Monto de la venta total más alta:", np.max(venta_total_tienda))
+
+# o) Calcule y muestre la media de ventas por tienda
+media_ventas_por_tienda = np.mean(matriz_valores, axis=1)
+print("Media de ventas por tienda:")
+print(media_ventas_por_tienda)
+
+# p) Encuentre y muestre la desviación estándar de las ventas en la categoría fútbol
+std_dev_futbol = np.std(matriz_valores[:, :3])
+print("Desviación estándar de las ventas en la categoría fútbol:", std_dev_futbol)
